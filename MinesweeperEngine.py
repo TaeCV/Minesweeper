9dab3feddeb53d1e34137b429e1b7915b17b40df
Chayakorn <70643030+TaeCV@users.noreply.github.com>
2020-12-28 00:52:08 +0700

    Add files via upload

---
+++ MinesweeperEngine.py
@@ -0,0 +1,84 @@
+"""
+This file will be about calculation the squares on board and others
+Coded by Chayakorn Vongbunsin
+Made on 27/12/2020
+"""
+
+import random
+
+class GameState():
+    def __init__(self, row, col, n):
+        self.row = row
+        self.col = col
+        self.NumsBomb = n
+        self.board = self.makeBoard(row, col, n)
+        self.ShowingBoard = [[False for j in range (col)] for i in range (row)]
+        self.FlagCheck = [[False for j in range (col)] for i in range (row)]
+        self.ClickBomb = False
+        self.ScoreBoard = self.MakingScoreBoard(row, col)
+        self.BoxLeft = row*col
+        self.Win = False 
+
+    def makeBoard(self, row, col, NumsBomb):
+        """
+        This function makes board fill with "-" and "*" which "-" is a blank and "*" is a bomb
+        """
+        board = [["-" for i in range (row)]for j in range (col)] # Make a blank board without bomb which "-" represents blank space
+        BombCount = 0
+        while BombCount < NumsBomb: # Fill in the bomb until the board has N bombs (N = NumsBomb)
+            RandRow = random.randint(0, row-1)
+            RandCol = random.randint(0, col-1)
+            while board[RandRow][RandCol] == "*":  # Check is it already a bomb in that square if it is change a row and column        
+                RandRow = random.randint(0, row-1)
+                RandCol = random.randint(0, col-1)
+            board[RandRow][RandCol] = "*"
+            BombCount += 1
+        
+        return board
+
+    def CheckOnBoard(self, row, col):
+        """
+        Check the square that you clicked and check neighbour box if its score equals to 0
+        """
+        if self.board[row][col] == "*":
+            self.ClickBomb = True
+        elif self.board[row][col] == "-":
+            self.ShowingBoard[row][col] = True
+            self.BoxLeft -= 1
+            if self.BoxLeft <= self.NumsBomb:
+                self.Win =True
+            if self.ScoreBoard[row][col] == 0: # If that box scores is not zero, we don't need to search more
+                self.CheckNearby(row, col)
+
+    def CheckNearby(self, row, col):
+        """
+        This function is recursive function to check all of 4 sides near the square that we looking for
+        """
+        Direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for PosX, PosY in Direction:
+            NewRow = row + PosX
+            NewCol = col + PosY
+            if 0 <= NewRow < self.row and 0 <= NewCol < self.col and self.board[NewRow][NewCol] == "-" and not self.ShowingBoard[NewRow][NewCol]:
+                self.ShowingBoard[NewRow][NewCol] = True
+                self.BoxLeft -= 1
+                if self.BoxLeft <= self.NumsBomb:
+                    self.Win =True
+                if self.ScoreBoard[NewRow][NewCol] == 0: # If that box scores is not zero, we don't need to search more
+                    self.CheckNearby(NewRow, NewCol)
+
+    def MakingScoreBoard(self, row, col):
+        """
+        This function will calculate each score box
+        """
+        TempBoard = [[0 for j in range (col)] for i in range (row)]
+        Direction = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1)]
+        for r in range (row):
+            for c in range (col):
+                if self.board[r][c] == "*":
+                    for PosX, PosY in Direction:
+                        NewRow = r + PosX
+                        NewCol = c + PosY
+                        if 0 <= NewRow < row and 0 <= NewCol < col and self.board[NewRow][NewCol] != "*":
+                            TempBoard[NewRow][NewCol] += 1
+        
+        return TempBoard
---
+++ MinesweeperMain.py
@@ -0,0 +1,183 @@
+"""
+This file represents the board minesweeper and get the action from the user
+Coded by Chayakorn Vongbunsin
+Made on 27/12/2020
+"""
+
+from os import path
+import pygame as pg
+import random
+import MinesweeperEngine
+import numpy as np
+
+"""
+Basic option. In the fututre add more option to add the width,height of the board and more functions
+"""
+
+#Given width, height, and FPS
+Width, Height = 600, 700
+FPS = 60
+
+#Set up basic option
+row = col = 15
+SQ_size = Width//row
+StartRow = 100
+StartCol = 0
+NumsBomb = 30
+Images = {}
+
+def LoadImages():    
+    filepath = path.dirname(__file__)
+    sourcepath = path.join(filepath, "Source")
+    Img = ["bomb", "flag", "cross", "block"]
+    for i in range (len(Img)):
+        Images[Img[i]] = pg.transform.scale(pg.image.load(path.join(sourcepath, Img[i] +".png")).convert_alpha(), (SQ_size, SQ_size))
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((Width,Height))
+    clock = pg.time.Clock()
+    screen.fill(pg.Color("white"))
+    LoadImages() # Loading Images
+    running = True #Boolean for the statement of the game
+    GameState = MinesweeperEngine.GameState(row, col, NumsBomb) # Set up a new board
+    TimeStart = pg.time.get_ticks()
+    Pause = False
+    TimeOver =0
+    addition_time = 0
+
+    while running:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                running = False
+            elif e.type == pg.MOUSEBUTTONDOWN:
+                if not GameState.ClickBomb or GameState.Win:                
+                    position = pg.mouse.get_pos()                
+                    x = (position[1]-100) // SQ_size
+                    y = position[0] // SQ_size
+                    if e.button == 1 and not GameState.FlagCheck[x][y]: # Left mouse click and not flaged
+                        GameState.CheckOnBoard(x, y)
+                    elif e.button == 3: # Right mouse click
+                        GameState.FlagCheck[x][y] = not GameState.FlagCheck[x][y]
+            elif e.type == pg.KEYDOWN:
+                if e.key == pg.K_r: # Reset the game
+                    GameState = MinesweeperEngine.GameState(row, col, NumsBomb) # Set up a new board
+                    TimeStart = pg.time.get_ticks()
+                    TimeOver = 0
+                elif e.key == pg.K_SPACE:
+                    Pause = not Pause
+                    if not Pause:
+                        TimeOver += pg.time.get_ticks()-addition_time
+                        addition_time = 0
+        screen.fill(pg.Color("white"))
+        drawBoardandLine(screen, GameState.board, GameState.FlagCheck, GameState.ShowingBoard, GameState.ScoreBoard)
+        if not Pause:
+            counting_time = pg.time.get_ticks() - TimeStart - TimeOver
+            # Change from millisecond to seconds and minutes
+            countingMinute = str(counting_time//60000).zfill(2)
+            countingSecond = str((counting_time % 60000) //1000).zfill(2)
+            countingString = "%s:%s" % (countingMinute,countingSecond)
+            font = pg.font.SysFont(None, 32)
+            counting_text = font.render(countingString, 0, pg.Color("black"))
+            counting_rect = counting_text.get_rect(center = (Width - 50, 50))
+        elif addition_time == 0:
+            addition_time = pg.time.get_ticks()
+        pg.draw.rect(screen, pg.Color("grey"), pg.Rect(Width-counting_text.get_width()//2-60, 25, counting_text.get_width()+20, counting_text.get_height()+25))
+        screen.blit(counting_text, counting_rect)
+        if GameState.ClickBomb:
+            ClickOnTheBomb(screen, GameState.board, (x,y), GameState.FlagCheck)
+            LossText(screen)
+            Pause = True
+        if GameState.Win:
+            WinnerText(screen)
+            Pause = True
+        clock.tick(FPS)
+        pg.display.flip()
+
+def drawBoardandLine(screen, board, FlagCheck, ShowingBoard, ScoreBoard):
+    """
+    This function draws a board and line on the screen by calling other functions
+    """
+    drawBoard(screen,board, FlagCheck, ShowingBoard, ScoreBoard)
+    drawLine(screen,board)
+
+def drawBoard(screen, board, FlagCheck, ShowingBoard, ScoreBoard):
+    """
+    This function draws a box on the board and also draw a flag if it is flagged 
+    """
+    Colors = ['', pg.Color("blue"), pg.Color("green"), pg.Color("red"), pg.Color("purple"),
+                pg.Color("maroon"), pg.Color("turquoise"), pg.Color("black"), pg.Color("gray")]
+    pg.draw.rect(screen, pg.Color("pink"), pg.Rect(0,0, Width, StartRow))
+    for r in range (row):
+        for c in range (col):
+            screen.blit(Images["block"], pg.Rect(StartCol + c * SQ_size, StartRow + r * SQ_size, SQ_size, SQ_size))
+            if FlagCheck[r][c]:
+                screen.blit(Images["flag"], pg.Rect(StartCol + c * SQ_size, StartRow + r * SQ_size, SQ_size, SQ_size))
+            if ShowingBoard[r][c]:
+                pg.draw.rect(screen, pg.Color("gray"), pg.Rect(StartCol + SQ_size*c, StartRow + SQ_size*r, SQ_size, SQ_size))
+                if ScoreBoard[r][c]:
+                    font = pg.font.SysFont("Helvitca", SQ_size*3//4, True, False)
+                    Text = font.render(str(ScoreBoard[r][c]), 0, Colors[ScoreBoard[r][c]])
+                    TextLocation = pg.Rect(StartCol + SQ_size*c, StartRow + SQ_size*r, SQ_size, SQ_size).move(SQ_size*3//8, SQ_size*3//8)
+                    screen.blit(Text, TextLocation)
+
+def drawLine(screen, board): 
+    """
+    This function draws a line on the board
+    """
+    pg.draw.line(screen, pg.Color("black"), (0, 0), (Width, 0), 4)
+    pg.draw.line(screen, pg.Color("black"), (0, StartRow-2), (Width, StartRow-2), 4)
+    pg.draw.line(screen, pg.Color("black"), (0, 0), (0, StartRow), 4)
+    pg.draw.line(screen, pg.Color("black"), (Width-2, 0), (Width-2, StartRow), 4)
+    for r in range (row):
+        pg.draw.line(screen, pg.Color("gainsboro"), (StartCol, StartRow + r*SQ_size), (StartCol + Width, StartRow + r*SQ_size))
+        for c in range (col):
+            pg.draw.line(screen, pg.Color("gainsboro"), (StartCol + c*SQ_size, StartRow), (StartCol + c*SQ_size, StartRow + Width))    
+    pg.draw.line(screen, pg.Color("gainsboro"), (StartCol, Height), (StartCol + Width, Height))
+
+
+def ClickOnTheBomb(screen, board, pos, FlagCheck):
+    """
+    This function will work if we click on the bomb.
+    It will highlight that square with red color and show other bombs on the board
+    If we flag wrong square, it will show a crossing bomb on the board
+    """
+    x,y = pos
+    s = pg.Surface((SQ_size, SQ_size))
+    s.fill(pg.Color("red"))
+    for r in range (row):
+        for c in range (col):
+            if board[r][c] == "*" and not FlagCheck[r][c]:
+                pg.draw.rect(screen, pg.Color("gray"), pg.Rect(StartCol + SQ_size*c, StartRow + SQ_size*r, SQ_size, SQ_size))
+                if r == x and c == y:
+                    screen.blit(s, (y*SQ_size, StartRow + x*SQ_size))
+                screen.blit(Images["bomb"], pg.Rect(StartCol + c * SQ_size, StartRow + r * SQ_size, SQ_size, SQ_size))
+            elif board[r][c] != "*" and FlagCheck[r][c]:
+                pg.draw.rect(screen, pg.Color("gray"), pg.Rect(StartCol + SQ_size*c, StartRow + SQ_size*r, SQ_size, SQ_size))
+                screen.blit(Images["bomb"], pg.Rect(StartCol + c * SQ_size, StartRow + r * SQ_size, SQ_size, SQ_size))
+                screen.blit(Images["cross"], pg.Rect(StartCol + c * SQ_size, StartRow + r * SQ_size, SQ_size, SQ_size))
+
+def WinnerText(screen):
+    text = "Congratulation, you are the winner !!"
+    font = pg.font.SysFont("Helvitca", 28, True, False)
+    Text = font.render(text, 0, pg.Color("black"))
+    Textbox = pg.Surface((Width//2 + 150, Width//2))
+    Textbox.set_alpha(150)
+    Textbox.fill(pg.Color("green"))
+    screen.blit(Textbox, (Width//4 -75 ,StartRow//2 + Width//4))
+    Text_rect = Text.get_rect(center = (Width//2, Height//2))
+    screen.blit(Text, Text_rect)
+
+def LossText(screen):
+    text = "Unlucky, you lost! :("
+    font = pg.font.SysFont("Helvitca", 28, True, False)
+    Text = font.render(text, 0, pg.Color("black"))
+    Textbox = pg.Surface((Width//2 + 150, Width//2))
+    Textbox.set_alpha(150)
+    Textbox.fill(pg.Color("green"))
+    screen.blit(Textbox, (Width//4 -75 ,StartRow//2 + Width//4))
+    Text_rect = Text.get_rect(center = (Width//2, Height//2))
+    screen.blit(Text, Text_rect)
+
+if __name__ == "__main__":
+    main()
